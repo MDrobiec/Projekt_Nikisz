2592f115a34fa8cce1b8672b143e74caf100b8e9
Mateusz <mateusz.drobiec@gmail.com>
2019-05-19 20:39:42 +0200

    Zaktualizowano błędy w museum activity

--- app/src/main/java/com/example/mateusz/nikisz/MuseumAcitivty.java
+++ app/src/main/java/com/example/mateusz/nikisz/MuseumAcitivty.java
@@ -1,44 +1,36 @@
 package com.example.mateusz.nikisz;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class MuseumAcitivty extends AppCompatActivity {
 
-    private TextView museum_textView_topText;
-    private TextView museumTextViewSmall;
-    private TextView museum_textView_description;
-    private TextView museum_textView_allText;
-    private TextView museum_textView_information;
-    private TextView museum_textView_localization;
-    private TextView museum_textView_localization_place;
-    private TextView museum_textView_localization_time;
-    private TextView show_distance_textView;
-
-    private Button show_distance;
-
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_museum);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         ResourceBundle rb = ResourceBundle.getBundle("textnikisz");
 
-        museum_textView_topText = (TextView) findViewById(R.id.museum_textView_topText);
-        museumTextViewSmall = (TextView) findViewById(R.id.museum_textView_smallText);
-        museum_textView_description = (TextView) findViewById(R.id.museum_textView_description);
-        museum_textView_allText = (TextView) findViewById(R.id.museum_textView_allText);
-        museum_textView_information = (TextView) findViewById(R.id.museum_textView_information);
-        museum_textView_localization = (TextView) findViewById(R.id.museum_textView_localization);
-        museum_textView_localization_place = (TextView) findViewById(R.id.museum_textView_localization_place);
-        museum_textView_localization_time = (TextView) findViewById(R.id.museum_textView_localization_time);
-        show_distance_textView = (TextView) findViewById(R.id.show_distance_textView);
+        TextView museum_textView_topText = findViewById(R.id.museum_textView_topText);
+        TextView museumTextViewSmall =  findViewById(R.id.museum_textView_smallText);
+        TextView museum_textView_description =  findViewById(R.id.museum_textView_description);
+        TextView museum_textView_allText =  findViewById(R.id.museum_textView_allText);
+        TextView museum_textView_information =  findViewById(R.id.museum_textView_information);
+        TextView museum_textView_localization =  findViewById(R.id.museum_textView_localization);
+        TextView museum_textView_localization_place =  findViewById(R.id.museum_textView_localization_place);
+        TextView museum_textView_localization_time =  findViewById(R.id.museum_textView_localization_time);
+
 
         museum_textView_topText.setText(rb.getString("museum_textView_topText"));
         museumTextViewSmall.setText(rb.getString("museum_textView_small"));
@@ -49,14 +41,13 @@ public class MuseumAcitivty extends AppCompatActivity {
         museum_textView_localization_place.setText(rb.getString("textView_localization_place"));
         museum_textView_localization_time.setText(rb.getString("museum_textView_localization_time"));
 
-        show_distance = (Button) findViewById(R.id.show_distance_button);
+        Button show_distance = findViewById(R.id.show_distance_button);
         show_distance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                show_distance_textView.setText("Działa");
+                TextView show_distance_textView = findViewById(R.id.show_distance_textView);
+                show_distance_textView.setText(R.string.good);
             }
         });
-
     }
-
 }
