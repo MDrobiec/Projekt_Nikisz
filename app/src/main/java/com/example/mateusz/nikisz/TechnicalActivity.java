7422b1b54fb3d5bb4bde8f69dbbb397cbc6b6417
Mateusz <mateusz.drobiec@gmail.com>
2019-06-06 21:37:52 +0200

    Konfiguracja layout technical

--- app/src/main/java/com/example/mateusz/nikisz/TechnicalActivity.java
+++ app/src/main/java/com/example/mateusz/nikisz/TechnicalActivity.java
@@ -1,7 +1,14 @@
 package com.example.mateusz.nikisz;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class TechnicalActivity extends AppCompatActivity {
 
@@ -9,6 +16,23 @@ public class TechnicalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_technical);
-        getSupportActionBar().hide();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Objects.requireNonNull(getSupportActionBar()).hide();
+        }
+
+        ResourceBundle rb = ResourceBundle.getBundle("textnikisz");
+
+        TextView technical_textView_top = findViewById(R.id.technical_textView_top);
+        TextView technical_textView_small = findViewById(R.id.technical_textView_small);
+        TextView technical_textView_small_info = findViewById(R.id.technical_textView_small_info);
+        TextView technical_textView_estateNikiszowiec = findViewById(R.id.technical_textView_estateNikiszowiec);
+        TextView technical_textView_nameOfWilson = findViewById(R.id.technical_textView_wilson);
+
+        technical_textView_top.setText(rb.getString("technical_textView_top"));
+        technical_textView_small.setText(rb.getString("technical_textView_small"));
+        technical_textView_small_info.setText(rb.getString("technical_textView_small_info"));
+        technical_textView_estateNikiszowiec.setText(rb.getString("technical_textView_nameOfEstate"));
+        technical_textView_nameOfWilson.setText(rb.getString("technical_textView_nameOfWilson"));
+
     }
 }
